aa56238abc430d2d5975dd3114d900a3897b17ec
KumarSahil03 <sahilkumar22699629@gmail.com>
2025-03-25 10:49:13 +0000

    different datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs ,not browser
+
+//make sure you have a good readalility in codes
+
+let name ="sahil"
+let age=18
+let isLoggedIn = false
+let state;
+
+
+/* number => 2^53
+bigint 
+string =>""
+boolean => true / false
+null => standalone value
+undefined =>
+symbol => when we talk about uniqueness
+
+
+object
+*/
+
+console.log(typeof age)
+console.log(typeof undefined);//undefined
